ac7565a882082f127a38728a08dbdc6e05f1a665
hemanthkuncha <168633321+hemanthkuncha@users.noreply.github.com>
2025-06-13 17:08:54 +0530

    Update taskServices.js

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://10.0.0.40:8080/api/tasks";
+const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
